460d7c0e4b73707a86f430e4ac00cd2314e5dab9
LeszekSte <34620437+LeszekSte@users.noreply.github.com>
2019-01-27 20:10:53 +0100

    Update Calk.java

--- src/Zadenie_6/Calk.java
+++ src/Zadenie_6/Calk.java
@@ -1,17 +1,27 @@
 package Zadenie_6;
-
 import java.util.Scanner;
 
 public class Calk {
     Scanner scanner = new Scanner(System.in);
-     Main main  = new Main();
+    Main main = new Main();
 
+    int wczytatwanieLiczby() {
+        int liczba;
 
-     int wczytatwanieLiczby(int i){
-            System.out.println("Proba nr" + ++i );
-        return scanner.nextInt();
+        do {
+            System.out.println("Podaj licbę ");
+            liczba = scanner.nextInt();
+            if (liczba < main.min) {
+                System.out.println("Podana liczba jest za mała");
+            } else if (liczba > main.max) {
+                System.out.println("Podana liczba jest za duża");
+            }
+        } while (liczba <= main.min && liczba >= main.max) ;
+        return liczba;
     }
 
-        
+        boolean DivisibleByThree ( int liczba){
+            return liczba % 3 == 0;
+        }
+    }
 
-}
